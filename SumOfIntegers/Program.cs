863b710f9239f9d2c6191a5af4547de297d8b5bc
Drew Boschert <drew.r.boschert@gmail.com>
2023-08-17 14:21:28 -0400

    Add project files.

---
+++ SumOfIntegers/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] nbrs = { 9, 1, 6, 9, 2, 4, 5, 3, 1, 3 };
+/*
+int sum = 0;
+
+sum += nbrs[0];
+sum += nbrs[1];
+sum += nbrs[2];
+sum += nbrs[3];
+sum += nbrs[4];
+sum += nbrs[5];
+sum += nbrs[6];
+sum += nbrs[7];
+sum += nbrs[8];
+sum += nbrs[9];
+
+    Console.WriteLine($"Sum is {sum}");
+int index = 0;
+int sum = nbrs[index]
+            + nbrs[++index]
+                + nbrs[++index]
+                + nbrs[++index]
+                + nbrs[++index]
+                + nbrs[++index]
+                + nbrs[++index]
+                + nbrs[++index]
+                + nbrs[++index]
+                + nbrs[++index];
+
+Console.WriteLine($"Sum is {sum}");
+Console.WriteLine("Average: " + (double)sum / nbrs.Length);
+*/
+
